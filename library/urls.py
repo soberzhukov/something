e8576fc3887a2f8ee430be0db53778370bfcd0e4
SoberZhukov <soberzhukov@localhost>
2022-05-03 19:55:17 +0300

    Базовый роутинг для приложения catalog

--- library/urls.py
+++ library/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ]
+from django.conf import settings
+from django.conf.urls.static import static
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
